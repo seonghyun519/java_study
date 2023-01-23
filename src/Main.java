919fd6b6b7c0aa7cf0a1c23af8243da1531435d9
SEONGHYUN PARK <sparklep519@gmail.com>
2023-01-23 23:57:26 +0900

    프로그래머스 코딩테스트/나머지값 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        programmers pg = new programmers();
+        int result = pg.solution(10,5);
+    }
+}
+
+class programmers {
+
+    int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
     }
 }
