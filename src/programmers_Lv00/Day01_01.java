00ba61a207fd5d50571350eb56adca5cf8242c30
SEONGHYUN PARK <sparklep519@gmail.com>
2023-01-25 23:56:35 +0900

    프로그래머스 코딩테스트/나머지값 구하기

---
+++ src/programmers_Lv00/Day01_01.java
@@ -0,0 +1,10 @@
+package programmers_Lv00;
+
+class Day01_01 {
+
+    int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }
+}
---
+++ src/programmers_Lv00/Day02_01.java
@@ -0,0 +1,8 @@
+package programmers_Lv00;
+
+public class Day02_01 {
+    int solution(int num1, int num2) {
+        int answer = 0;
+        return num1 - num2;
+    }
+}
