ad0b19a73376f7ca2f37c623b16d0c25b5f581fb
SEONGHYUN PARK <sparklep519@gmail.com>
2023-02-01 09:56:04 +0900

    20230201

--- src/etc/Main.java
+++ src/etc/Main.java
@@ -1,8 +1,14 @@
 package etc;
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("값을 입력하세요");
+        StringBuilder t = new StringBuilder(scanner.nextLine());
+        int sum = 0;
+        for (int i = 0; i < t.length(); i++) {
+            System.out.println(t[i]);
+        }
 
     }
 }
