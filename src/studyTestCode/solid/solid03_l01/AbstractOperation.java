ae92b0a1314d91a74c24f79b8b427492a525356c
SEONGHYUN PARK <sparklep519@gmail.com>
2023-02-03 22:21:29 +0900

    solid update 230203

---
+++ src/studyTestCode/solid/solid03_l01/AbstractOperation.java
@@ -0,0 +1,7 @@
+package studyTestCode.solid.solid03_l01;
+
+public abstract class AbstractOperation {
+    public abstract int operate(int firstNumber, int secondNumber);
+
+    public abstract boolean isInvalid(int secondNumber);
+}
